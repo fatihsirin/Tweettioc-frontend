4929cbafd5e6fcd51c3935c8d04323ae4d31661d
Fatih Sirin <fsirin@ebay.com>
2022-01-19 14:44:54 +0300

    -

--- app/views.py
+++ app/views.py
@@ -13,36 +13,44 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 import datetime
 
-# client = pymongo.MongoClient("mongodb://localhost:27017/")
-# db = client.ioctest  # Select the database
-# ioc = db.ioc
-
-
 from app.models import *
 
 
 @login_required(login_url="/login/")
 def index(request):
-    user_tweets = Tweet.objects.filter(date__gt=datetime.datetime.strptime("2021-12-25", "%Y-%m-%d"),
-                                       date__lt=datetime.datetime.strptime("2021-12-30", "%Y-%m-%d"))
+    # user_tweets = Tweet.objects.filter(date__gt=datetime.datetime.strptime("2021-12-25", "%Y-%m-%d"),
+    #                                    date__lt=datetime.datetime.strptime("2021-12-30", "%Y-%m-%d"))
 
-    _temp = Dashboard.objects.filter(type="iocCounts").order_by("-date")[0]
-    counts = _temp.data[0]
+    iocCounts = Dashboard.objects.filter(type="iocCounts").order_by("-date")[0]
+    iocCounts = iocCounts.data[0]
 
     counts_daily = Dashboard.objects.filter(type="daily").order_by("-date")[0].data
     counts_monthly = Dashboard.objects.filter(type="monthly").order_by("-date")[0].data
+    daily_hashtags = Dashboard.objects.filter(type="hashtagsDaily").order_by("-date")[0].data
+    all_hashtags = Dashboard.objects.filter(type="hashtagsAll").order_by("-date")[0].data
 
+    object_list = Tweet.objects.filter(date__gt=datetime.datetime.now() - datetime.timedelta(days=7),
+                                       date__lt=datetime.datetime.now()).order_by('date')
 
-    query = [{"$match": {"type": "iocCounts"}},
-             {"$sort": {"date": 1}},
-             {"$limit": 1}, ]
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get('page', 1)
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
+    page_range = paginator.get_elided_page_range(page, on_each_side=3, on_ends=3)
 
     context = {
-        'counts': counts, 'counts_weekly':counts_daily,
-        'counts_monthly':counts_monthly
+        'iocCounts': iocCounts, 'counts_weekly': counts_daily,
+        'counts_monthly': counts_monthly, 'daily_hashtags': daily_hashtags,
+        'all_hashtags': all_hashtags, 'page_obj': page_obj, 'page_range': page_range,
+        'page': page, 'paginator': paginator,
     }
 
-    return render(request, "index.html",context)
+    return render(request, "index.html", locals())
 
 
 @login_required(login_url="/login/")
